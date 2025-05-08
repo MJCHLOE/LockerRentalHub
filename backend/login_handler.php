fe484f055be22e15436a75bcb24ee91264c546cd
MJCHLOE <lumapaskirkmj@gmail.com>
2025-05-08 14:45:01 +0800

    .

--- backend/login_handler.php
+++ backend/login_handler.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require '../db/database.php';
+require_once '../db/database.php';
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $username = trim($_POST["username"]);
