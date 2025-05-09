b14b67d3a63e924405ab9412c419032069a3607c
MJCHLOE <lumapaskirkmj@gmail.com>
2025-05-09 13:51:05 +0800

    .

--- admin_and_staff_backend/fetch_rentals.php
+++ admin_and_staff_backend/fetch_rentals.php
@@ -31,7 +31,7 @@ try {
         echo "<td>{$row['rental_id']}</td>";
         echo "<td>{$row['client_name']}</td>";
         echo "<td>{$row['locker_id']}</td>";
-        echo "<td>" . date('Y-m-d H:i', strtotime($row['rental_date'])) . "</td>";
+        echo "<td>" . date('Y-m-d H:i') . "</td>";
         
         // Add specific classes for different statuses
         $statusClass = '';
