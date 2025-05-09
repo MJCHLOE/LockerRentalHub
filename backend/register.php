29133db7b29601e9041e4e54075a8cc6655fe1e6
MJCHLOE <lumapaskirkmj@gmail.com>
2025-05-09 10:12:18 +0800

    .

--- backend/register.php
+++ backend/register.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once "../db/database.php";
+require "../db/database.php";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $username = trim($_POST["username"]);
