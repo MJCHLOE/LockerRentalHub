c837997afc11d6a938bad21110355d6beb663917
MJCHLOE <lumapaskirkmj@gmail.com>
2025-05-09 23:00:11 +0800

    Test change

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -212,7 +212,7 @@
 
       <!-- Displaying all users details -->  
       <div class="table-responsive bg-dark text-white p-3 rounded">
-        <table class="table table-dark table-bordered">
+        <table class="table-scrolled table-dark table-bordered">
           <thead>
             <tr>
               <th>ID</th>
