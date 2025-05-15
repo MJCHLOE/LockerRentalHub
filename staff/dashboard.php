0216aad52e6e499d5a5d1a8544c61ec5461274f4
MJCHLOE <lumapaskirkmj@gmail.com>
2025-05-15 10:45:45 +0800

    .

--- staff/dashboard.php
+++ staff/dashboard.php
@@ -78,7 +78,7 @@ if (isset($_GET['success'])) {
                     <th>Phone</th>
                 </tr>
                 </thead>
-                <tbody id="clientsTableBody table-scrollable">
+                <tbody id="clientsTableBody">
                 <?php include '../staff_backend/fetch_clients.php'; ?>
                 </tbody>
             </table>
